7e44ba03e323df3640870d9808ccca1c8e4c1fc1
nguyenlejoe <joe_le.1@hotmail.com>
2024-02-28 10:50:43 -0800

    update GRK and add dfk

--- app/tx/[chain]/[tx_hash]/page.tsx
+++ app/tx/[chain]/[tx_hash]/page.tsx
@@ -6,6 +6,7 @@ import { TransactionReceiptView } from "@covalenthq/goldrush-kit"
 import { Flex } from "@radix-ui/themes"
 import bg from "../../../../static/dfk-frame.png"
 import { Button } from "@/components/ui/button"
+import Image from "next/image"
 
 export default function TX({
   params,
@@ -16,8 +17,9 @@ export default function TX({
 
   return (
     <div className="w-full flex items-center flex-col gap-4">
-      <div className=" flex h-[75rem] items-center justify-center bg-cover" style={{ backgroundImage: `url(${bg.src})`, backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
-        <div className="flex  justify-center h-[52rem] w-[55rem] overflow-auto ml-2">
+      <div className=" flex h-[75rem] w-[55rem] items-center justify-center bg-cover" style={{ backgroundImage: `url(${bg.src})`, backgroundRepeat: "no-repeat", backgroundPosition: "center"}}>
+        <div className="h-[52rem] w-[36rem] overflow-auto ml-2 relative">
+          <Image src={"/static/dfk.png"} alt={""} width={60} height={60} className="absolute right-6 top-[3rem]"/>
           <TransactionReceiptView
             chain_name={params.chain}
             tx_hash={params.tx_hash}
